d7b1bdbea01b4294ba9900a827ed6f1d020ca2f2
苏鹏飞 <supengfei@supengfeideMacBook-Pro.local>
2018-12-01 22:38:30 +0800

    mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
+const mongoose =require('mongoose')
 var router = express.Router();
 
+
+const DB_URL = 'mongodb://127.0.0.1:27017'
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected',function(){
+  console.log('mongo connect success')
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
